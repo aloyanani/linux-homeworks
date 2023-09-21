8317bb32c168b2c1582fdadb82058348d49491de
aloyanani <144101514+aloyanani@users.noreply.github.com>
2023-09-21 16:37:03 +0400

    Create complex.cpp

---
+++ ENV-2/complex.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include "complex.h"
+
+
+   // Overload + operator for complex numbers
+   Complex Complex::operator+(const Complex& c1)
+   {
+     return Complex(a + c1.a, b + c1.b);
+   }
+
+   // Overload - operator for complex numbers
+   Complex Complex::operator-(const Complex& c2)
+   {
+     return Complex(a - c2.a, b - c2.b);
+   }
+
+   // Overload * operator to multiply a complex number by a constant
+   Complex Complex::operator*(int num)
+   {
+     return Complex(a * num , b * num);
+   }
+
+
+
