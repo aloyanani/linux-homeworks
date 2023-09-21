b46b2f747258885b237b08e016349167b3d66034
aloyanani <144101514+aloyanani@users.noreply.github.com>
2023-09-21 16:34:28 +0400

    Update complex (1).h

--- ENV-2/complex (1).h
+++ ENV-2/complex (1).h
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include <vector>
-
 
 class Complex
 {
@@ -13,32 +11,25 @@ public:
 
    // Member initializer list
    Complex(int a, int b): a(a), b(b) {}
+
    // Overload + operator for complex numbers
-   Complex operator+(const Complex& c1)
-   {
-     return Complex(a + c1.a, b + c1.b);
-   }
+   Complex operator+(const Complex& c1);
+
    // Overload - operator for complex numbers
-   Complex operator-(const Complex& c2)
-   {
-     return Complex(a - c2.a, b - c2.b);
-   }
+   Complex operator-(const Complex& c2);
+
    // Overload * operator to multiply a complex number by a constant
-   Complex operator*(int num)
-   {
-     return Complex(a * num , b * num);
-   }
+   Complex operator*(int num);
+
    // A function that returns the absolute value of a complex number
    double absolute_value()
    {
      return sqrt(a * a + b * b);
    }
-   
    friend std::ostream& operator<<(std::ostream& out, const Complex& c)
    {
       out << "(" << c.a << ", " << c.b << ")";
       return out;
    }
-
 
 };
