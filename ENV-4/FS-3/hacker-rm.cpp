b0a57e870fd41990cd9ae38644036f40085b9d5c
Ani <144101514+aloyanani@users.noreply.github.com>
2023-10-23 19:36:37 +0400

    Add files

---
+++ ENV-4/FS-3/hacker-rm.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+int main(int argc, char** argv)
+{
+    // Check if there are not enough command-line arguments.
+    if(argc < 2)
+    {
+       std::cerr << "Provide file path." << std::endl;
+       exit(1);
+    }
+
+    char* file = argv[1];
+
+    // Open the file in read-only mode.
+    int fd = open(file, O_RDONLY | O_WRONLY | O_TRUNC);
+   
+     // Check if the file couldn't be opened.
+    if(fd < 0)
+    {
+        std::cerr << strerror(errno) << std::endl;
+        exit(errno);
+    }
+
+    // Check information about the file.
+    struct stat file_info ;
+    if(stat(file, &file_info) != 0)
+    {
+        std::cerr << strerror(errno) << std::endl;
+        exit(errno);
+    }
+
+    int file_size = file_info.st_size;
+
+    int i = 0;
+    char file_delete = '\0';
+
+    // Overwrite the file with null bytes.
+    while( i < file_size)
+    {
+          
+        ssize_t write_bytes = write(fd, &file_delete, 1);
+        if(write_bytes < 0)
+        {
+            std::cerr << strerror(errno) << std::endl;
+            exit(errno);
+
+        }
+
+        ++i;
+
+    }
+
+    // Devete the file from the file system.
+    int unlink_file = unlink(file);
+    if(unlink_file < 0)
+        {
+            std::cerr << strerror(errno) << std::endl;
+            exit(errno);
+
+        }
+
+    // close the file.
+    int close_file = close(fd);
+    if(close_file < 0)
+    {
+        std::cerr << strerror(errno) << std::endl;
+        exit(errno);
+    }
+    return 0;
+}
